19fdcbeb8cf0ef38923ce88603ae6b29e6f4c015
RNKARKI <113538012+RNkarki@users.noreply.github.com>
2024-05-17 22:06:17 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const menuToggler = document.querySelectorAll("#menu_toggle");
 menuToggler.forEach(toggler => {
   toggler.addEventListener("click", () => header.classList.toggle("showMenu"));
 });
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal')
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+     //let revealPoint = 50;
+
+    if (revealTop < windowHeight) {
+      reveals[i].classList.add('active')
+    } else {
+      reveals[i].classList.remove('active')
+    }
+  }
+}
